d4e1544389c23c8887be14e489ea912189ab8216
studentai <studentai@192.168.31.101>
2018-01-18 14:34:02 +0100

    Initial build

---
+++ src/statistikberechnung/StatistikBerechnung.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistikberechnung;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ *
+ * @author studentai
+ */
+public class StatistikBerechnung {
+    Random rg = new Random();
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        StatistikBerechnung sbr = new StatistikBerechnung();
+        int[] werte = sbr.generate(1000000);
+        
+        int median = sbr.median(werte);
+        System.out.println("Median = " + median);        
+        
+        int spannweite = sbr.weite(werte);
+        System.out.println("Spannweite = " + spannweite);
+        
+        int zaehlen = 0; // hier zaehlen wir die Anzahl für einen wert
+        
+        for (int i = 0; i < werte.length; i++) {
+            int wert = werte[i];
+            if (wert == 10) {
+                zaehlen++;                      
+            }
+            
+        }
+        
+        double arithemtischesMittel = sbr.arithMittel(werte);
+        
+        System.out.println("Gezaehlter Wert = " + zaehlen);
+        System.out.println("Arithmetischer Mittelwert = " + arithemtischesMittel);
+        
+    }
+
+    private int median(int[] werte) {
+        Arrays.sort(werte);
+        
+        return (werte[werte.length / 2]);
+    }
+
+    private int[] generate(int anzahl) {
+        int[] result = new int[anzahl];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = rg.nextInt(100) + 1;    
+        }
+        return result;
+    }
+
+    private double arithMittel(int[] werte) {
+        double summe = 0;
+        for (int i = 0; i < werte.length; i++) {
+            int wert = werte[i];
+            summe += wert;
+        }
+        
+        return (summe / werte.length);
+    }
+
+    private int weite(int[] werte) {
+        Arrays.sort(werte);
+        return (werte[werte.length-1] - werte[0]);
+    }
+    
+}
