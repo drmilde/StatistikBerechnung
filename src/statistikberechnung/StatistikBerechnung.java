a9acf55d8c3f2489af94a06aec6b7f0b1419b740
studentai <studentai@192.168.31.101>
2018-01-18 15:10:18 +0100

    generate Gauss eingefügt

--- src/statistikberechnung/StatistikBerechnung.java
+++ src/statistikberechnung/StatistikBerechnung.java
@@ -13,6 +13,7 @@ import java.util.Random;
  * @author studentai
  */
 public class StatistikBerechnung {
+
     Random rg = new Random();
 
     /**
@@ -21,40 +22,41 @@ public class StatistikBerechnung {
     public static void main(String[] args) {
         StatistikBerechnung sbr = new StatistikBerechnung();
         int[] werte = sbr.generate(1000000);
-        
+        int[] werteNormal = sbr.generateGauss(1000000);
+
         int median = sbr.median(werte);
-        System.out.println("Median = " + median);        
-        
+        System.out.println("Median = " + median);
+
         int spannweite = sbr.weite(werte);
         System.out.println("Spannweite = " + spannweite);
-        
+
         int zaehlen = 0; // hier zaehlen wir die Anzahl für einen wert
-        
+
         for (int i = 0; i < werte.length; i++) {
             int wert = werte[i];
             if (wert == 10) {
-                zaehlen++;                      
+                zaehlen++;
             }
-            
+
         }
-        
+
         double arithemtischesMittel = sbr.arithMittel(werte);
-        
+
         System.out.println("Gezaehlter Wert = " + zaehlen);
         System.out.println("Arithmetischer Mittelwert = " + arithemtischesMittel);
-        
+
     }
 
     private int median(int[] werte) {
         Arrays.sort(werte);
-        
+
         return (werte[werte.length / 2]);
     }
 
     private int[] generate(int anzahl) {
         int[] result = new int[anzahl];
         for (int i = 0; i < result.length; i++) {
-            result[i] = rg.nextInt(100) + 1;    
+            result[i] = rg.nextInt(100) + 1;
         }
         return result;
     }
@@ -65,13 +67,21 @@ public class StatistikBerechnung {
             int wert = werte[i];
             summe += wert;
         }
-        
+
         return (summe / werte.length);
     }
 
     private int weite(int[] werte) {
         Arrays.sort(werte);
-        return (werte[werte.length-1] - werte[0]);
+        return (werte[werte.length - 1] - werte[0]);
     }
-    
+
+    private int[] generateGauss(int anzahl) {
+        int[] result = new int[anzahl];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = rg.nextInt(100) + 1;
+        }
+        return result;
+    }
+
 }
