f7373feb0668dbde4fe62a3c59b478154c603011
Micha Heiderich <micha.heiderich@ai.hs-fulda.de>
2018-01-18 15:51:43 +0100

    made vars in generateGauss function final added a view comments

--- src/statistikberechnung/StatistikBerechnung.java
+++ src/statistikberechnung/StatistikBerechnung.java
@@ -9,92 +9,103 @@ import java.util.Arrays;
 import java.util.Random;
 
 /**
- *
- * @author studentai
+ * @author drmilde
  */
-public class StatistikBerechnung {
-
+public class StatistikBerechnung
+{
+    
     Random rg = new Random();
-
-    // added new line for testing
+    
     /**
-     * @param args the command line arguments
+     * @param args
+     *         the command line arguments
      */
-    public static void main(String[] args) {
-        StatistikBerechnung sbr = new StatistikBerechnung();
-        int[] werte = sbr.generate(1000000);
-        int[] werteNormal = sbr.generateGauss(1000000);
+    public static void main(String[] args)
+    {
+        StatistikBerechnung sbr         = new StatistikBerechnung();
+        int[]               werte       = sbr.generate(1000000);
+        int[]               werteNormal = sbr.generateGauss(1000000);
         //Das Array wird nie verwendet
-
+        
         int median = sbr.median(werte);
         System.out.println("Median = " + median);
-
+        
         int spannweite = sbr.weite(werte);
         System.out.println("Spannweite = " + spannweite);
-
+        
         int zaehlen = 0; // hier zaehlen wir die Anzahl für einen wert
-
-        for (int i = 0; i < werte.length; i++) {
+        
+        for (int i = 0; i < werte.length; i++)
+        {
             int wert = werte[i];
-            if (wert == 10) {
+            if (wert == 10)
+            {
                 zaehlen++;
             }
-
+            
         }
-
+        
         double arithemtischesMittel = sbr.arithMittel(werte);
-
+        
         System.out.println("Gezaehlter Wert = " + zaehlen);
         System.out.println("Arithmetischer Mittelwert = " + arithemtischesMittel);
-
+        
     }
-
-    private int median(int[] werte) {
+    
+    private int median(int[] werte)
+    {
         Arrays.sort(werte);
-
+        
         return (werte[werte.length / 2]);
     }
-
-    private int[] generate(int anzahl) {
+    
+    private int[] generate(int anzahl)
+    {
         int[] result = new int[anzahl];
-        for (int i = 0; i < result.length; i++) {
+        for (int i = 0; i < result.length; i++)
+        {
             result[i] = rg.nextInt(100) + 1;
         }
         return result;
     }
-
-    private double arithMittel(int[] werte) {
+    
+    private double arithMittel(int[] werte)
+    {
         double summe = 0;
-        for (int i = 0; i < werte.length; i++) {
+        for (int i = 0; i < werte.length; i++)
+        {
             int wert = werte[i];
             summe += wert;
         }
-
+        
         return (summe / werte.length);
     }
-
-    private int weite(int[] werte) {
+    
+    private int weite(int[] werte)
+    {
         Arrays.sort(werte);
         return (werte[werte.length - 1] - werte[0]);
     }
-
-    private int[] generateGauss(int anzahl) {
+    
+    private int[] generateGauss(int anzahl)
+    {
         int[] result = new int[anzahl];
         
-        double sd = 20;
-        double mju = 50;
+        final double sd  = 20;
+        final double mju = 50;
         
-        for (int i = 0; i < result.length; i++) {
-            int z1 = rg.nextInt(100) + 1;
-            int z2 = rg.nextInt(100) + 1;
-            
-            double val1 = Math.sqrt(-2 * Math.log(z1));
-            double val2 = Math.sin(2 * Math.PI * z2);
-            double z = sd * val1 * val2 + mju;
-                        
-            result[i] = (int)Math.round(z);
+        for (int i = 0; i < result.length; i++)
+        {
+            double val1 = Math.sqrt(-2 * Math.log(rg.nextInt(100) + 1));
+            double val2  = Math.sin(2 * Math.PI * rg.nextInt(100) + 1);
+            double z =
+                    sd * // standard deviation
+                    val1 * // value 1
+                    val2 + // value 2
+                    mju; // mean
+            result[i] = (int) Math.round(z);
         }
         return result;
     }
-
+    
 }
